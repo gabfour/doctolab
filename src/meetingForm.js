2ccbdaa2abe2c1c89920d3139f40c341b221cea4
gabfour <gabfourtoy@gmail.com>
2023-03-09 10:54:02 +0100

    Date and time adjustments

--- src/meetingForm.js
+++ src/meetingForm.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { useSignOut } from 'react-firebase-hooks/auth';
 import { Button, TextField } from '@mui/material';
 import { auth } from './index';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { TimeField } from '@mui/x-date-pickers/TimeField';
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -12,7 +12,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 export function MeetingForm() {
 
     const [date, setDate] = useState(dayjs('2022-04-17'));
-    const [heure, setHeure] = useState(dayjs('T15:30'));
+    const [heure, setHeure] = useState(dayjs('2022-04-17T15:30'));
     const [signOut] = useSignOut(auth); // loading, error
 
     const SignUserOut = () => signOut(auth);
@@ -21,8 +21,8 @@ export function MeetingForm() {
         <><form>
             <TextField id="outlined-basic" label="Nom" variant="outlined" />
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker label="Selection de la date"value={date} onChange={(nouvelleDate) => setDate(nouvelleDate)} />
-                <TimePicker label="Selection de l'heure" value={heure} onChange={(nouvelleHeure) => setHeure(nouvelleHeure)} />
+                <MobileDatePicker label="Selection de la date"value={date} onChange={(nouvelleDate) => setDate(nouvelleDate)} />
+                <TimeField label="Selection de l'heure" value={heure} onChange={(nouvelleHeure) => setHeure(nouvelleHeure)} format="HH:mm" />
             </LocalizationProvider>
         </form>
         <Button onClick={SignUserOut}>Sign Out</Button></>
